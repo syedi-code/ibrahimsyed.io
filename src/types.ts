d9cf90fbbb99db4701b10491dd113b26418d8419
Ibrahim Syed <syedi@oregonstate.edu>
2025-05-30 23:53:10 -0700

    album links

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface AlbumLink {
+  service: 'spotify' | 'appleMusic' | 'youtubeMusic';
+  url: string;
+}
+
+export interface Album {
+  id: string;
+  title: string;
+  artist: string;
+  year: number;
+  albumArtSrc: string; // Path to publicly served album art, e.g., /img/album/artwork.png
+  backgroundSrc: string; // Path to background image, e.g., /src/assets/img/background.png
+  links: AlbumLink[]; // Array of links to the album on different services
+}
